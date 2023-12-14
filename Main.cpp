6fcec36d5bd27f218829e2de1e7437da46dd8f1c
allo <51132972+thatalloguy@users.noreply.github.com>
2023-12-14 20:56:27 +0100

    updated the main file
    i forgor 💀

--- Main.cpp
+++ Main.cpp
@@ -142,9 +142,11 @@ int main() {
     sceneManager.createNewScene("mainScene");
     sceneManager.setCurrentScene("mainScene");
 
+    //Load demo scene
+
     GameObject helmet(glm::vec3(0, -5.0f, 0), glm::quat(1.0f, 0.0, 0.0, 0.0), glm::vec3(3.0f, -3.0f, 3.0f), "Helmet :)");
     helmet.createModel("../../Resources/Model/Helmet/DamagedHelmet.gltf");
-
+    helmet.model.mMaterial.roughnessAmplifier = 0.0f;
     GameObject ground(glm::vec3(0.0f, 5.0f, 0.0f), glm::quat(1.0f, 0.0f, 0.0f, 0.0f), glm::vec3(10.0f, -1.0f, 10.0f), "Ground");
 
     ground.createModel("../../Resources/Model/testCube/testCube.gltf");
